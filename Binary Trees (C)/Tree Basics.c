4226379f049bd490c3a3547e6dc4a1b96695702e
Josh Dietrich <59072117+Taggagii@users.noreply.github.com>
2020-11-22 12:41:45 -0500

    Commented code

--- Binary Trees (C)/Tree Basics.c
+++ Binary Trees (C)/Tree Basics.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//general stucture for the nodes of the tree
 struct node_S
 {
     int value;
@@ -9,31 +10,39 @@ struct node_S
 };
 typedef struct node_S Node;
 
-Node *newNode(int data)
+//generates a new node holding the value as input
+Node *newNode(int value)
 {
     Node *node = (Node*) malloc(sizeof(Node));
 
-    node->value = data;
+    node->value = value;
     node->left = NULL;
     node->right = NULL;
     return node;
 }
 
+//prints the tree recursively (starting right and moving back)
 void printTreeFunctional(Node *root, int depth)
 {   
+    //leave when end is reached
     if (root == NULL) return;
+    //recursively move all the way to the far right
     printTreeFunctional(root->right, depth + 1);
+    //after the far right / left is reached, print out the value of this leaf
     for (int i = 0; i < depth; i++)
         printf("\t");
     printf("%d\n", root->value);
+    //recursively move to the far left (from the current level)
     printTreeFunctional(root->left, depth + 1);
 }
 
+//allows the user to have an easy input function
 void printTree(Node *root)
 {
     printTreeFunctional(root, 0);
 }
 
+//generates a tree of indexes (to be updated). Starts left and moves right using rule 1 from "General Rules of Binary Trees"
 void makeTreeFunctional(Node *root, int level, int maxLevel)
 {
     if (level == maxLevel) return;
@@ -41,15 +50,19 @@ void makeTreeFunctional(Node *root, int level, int maxLevel)
     makeTreeFunctional(root->right = newNode(2 * root->value + 2), level + 1, maxLevel);
 }
 
+//allows the user to have an easy input function
 Node *makeTree(int levels)
 {
+    //generating a root to branch from
     Node *root = newNode(0);
+    //recusively building the tree
     makeTreeFunctional(root, 0, levels - 1);
     return root;
 }
 
 int main()
 {
+    //simple usage of above concepts
     Node *root = makeTree(4);
     printTree(root);
     return 0;
