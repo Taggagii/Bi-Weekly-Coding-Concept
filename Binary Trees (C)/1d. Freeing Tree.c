e9f5782f291d5a265ca60ca927c826aaeacad5ad
Josh Dietrich <59072117+Taggagii@users.noreply.github.com>
2020-11-24 19:04:51 -0500

    Update 1d. Freeing Tree.c

--- Binary Trees (C)/1d. Freeing Tree.c
+++ Binary Trees (C)/1d. Freeing Tree.c
@@ -1,5 +1,6 @@
 #include "1a. Tree Basics.h"
 
+//dropping into the tree and deleting when we reach a leaf
 void freeTree(Node *root)
 {
     if (root == NULL) return;
@@ -11,9 +12,14 @@ void freeTree(Node *root)
 
 int main()
 {
+    //showing the creating and deletion of tree
     Node *root = makeTree(5);
     printTree(root);
     freeTree(root);
     printf("%d\n", root->value); //output shows the memory is no longer allocated
+    /*
+        Printing the entire tree would not work, as C doesn't recognize when the value has been free and simply believes it a pointer — when
+        printed, the tree appears the print the entirety of lined up memory
+    */
     return 0;
 }
